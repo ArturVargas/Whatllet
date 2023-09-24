78229525ec07e475b5e2dd15379b55f6d9c0c7f8
Diego Jimenez <D9J9V@MC-Artua.local>
2023-09-23 21:55:04 -0600

    Working on credit score vector settings

--- packages/nextjs/components/example-ui/ContractInteraction.tsx
+++ packages/nextjs/components/example-ui/ContractInteraction.tsx
@@ -27,18 +27,19 @@ export const ContractInteraction = () => {
       <div className="flex flex-col w-full mx-5 sm:mx-8 2xl:mx-20">
         <div className={`mt-10 flex gap-2 ${visible ? "" : "invisible"} max-w-2xl`}>
           <div className="flex gap-5 bg-base-200 bg-opacity-80 z-0 p-7 rounded-2xl shadow-lg">
-            <span className="text-3xl">👋🏻</span>
+            <span className="text-3xl">⚙️</span>
             <div>
               <div>
-                In this page you can see how some of our <strong>hooks & components</strong> work, and how you can bring
-                them to life with your own design! Have fun and try it out!
+                In this page you can set up your
+                <strong> credit profile </strong>
+                for the lending functionalities.
               </div>
-              <div className="mt-2">
-                Check out{" "}
-                <code className="italic bg-base-300 text-base font-bold [word-spacing:-0.5rem]">
-                  packages / nextjs/pages / example-ui.tsx
-                </code>{" "}
-                and its underlying components.
+            </div>
+            <span className="text-3xl">☸️</span>
+            <div>
+              <div>
+                Whatllet lending protocol operates on a<strong> prosumer model </strong>
+                your rules apply for both lending and borrowing.
               </div>
             </div>
           </div>
@@ -51,7 +52,7 @@ export const ContractInteraction = () => {
         </div>
 
         <div className="flex flex-col mt-6 px-7 py-8 bg-base-200 opacity-80 rounded-2xl shadow-lg border-2 border-primary">
-          <span className="text-4xl sm:text-6xl text-black">Set a Greeting_</span>
+          <span className="text-4xl sm:text-6xl text-black">Set your rules</span>
 
           <div className="mt-8 flex flex-col sm:flex-row items-start sm:items-center gap-2 sm:gap-5">
             <input
@@ -60,6 +61,13 @@ export const ContractInteraction = () => {
               className="input font-bai-jamjuree w-full px-5 bg-[url('/assets/gradient-bg.png')] bg-[length:100%_100%] border border-primary text-lg sm:text-2xl placeholder-white uppercase"
               onChange={e => setNewGreeting(e.target.value)}
             />
+            {/*
+            <input
+              type="bool"
+
+
+            />
+             */}
             <div className="flex rounded-full border border-primary p-1 flex-shrink-0">
               <div className="flex rounded-full border-2 border-primary p-1">
                 <button
